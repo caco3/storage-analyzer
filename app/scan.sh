d7f72f061f491ba4506b7e385a396603c3a0e7dc
CaCO3 <caco@ruinelli.ch>
2025-12-29 16:28:09 +0100

    create DB in temp folder

--- app/scan.sh
+++ app/scan.sh
@@ -2,7 +2,12 @@
 
 set -euo pipefail
 
-DATABASE="/database/duc_"`date +"%Y-%m-%d_%H-%M-%S".db`
+
+DATABASE_FOLDER="database"
+DATABASE_FOLDER_TEMP="database/temp"
+DATABASE_FILE="duc_"`date +"%Y-%m-%d_%H-%M-%S".db`
+
+
 
 LOG_FILE="${DUC_LOG_FILE:-/var/log/duc.log}"
 LOCK_DIR="/tmp/scan.lock"
@@ -16,27 +21,28 @@ if mkdir "$LOCK_DIR" 2>/dev/null; then
     trap 'rm -rf "$LOCK_DIR"' EXIT
 
     {
-        echo "Start of scan: $(date) (Database: $DATABASE)"
-        /usr/local/bin/duc index --progress /scan -d $DATABASE --check-hard-links ${EXCLUDE[@]}
+        mkdir -p $DATABASE_FOLDER
+        mkdir -p $DATABASE_FOLDER_TEMP
+        echo "Start of scan: $(date) (Database: $DATABASE_FILE)"
+        /usr/local/bin/duc index --progress /scan -d $DATABASE_FOLDER_TEMP/$DATABASE_FILE --check-hard-links ${EXCLUDE[@]}
         # --exclude=Selektion --exclude=Speziell --exclude=roms
         status=$?
         echo "End of scan: $(date) (exit code: $status)"
 
         # Data Size
         # 2025-12-29 08:06:01 2785165  343721 390117691392 /scan
-        DATA_SIZE=`/usr/local/bin/duc info -d $DATABASE -b | tail -n 1 | awk '{print $5}'`
+        DATA_SIZE=`/usr/local/bin/duc info -d $DATABASE_FOLDER_TEMP/$DATABASE_FILE -b | tail -n 1 | awk '{print $5}'`
         echo "Data size: $((DATA_SIZE / 1024 / 1024)) MB ($DATA_SIZE bytes)"
         # Rename database to include data size
-        NEW_DATABASE="${DATABASE%.db}_${DATA_SIZE}.db"
-        mv "$DATABASE" "$NEW_DATABASE"
-        DATABASE="$NEW_DATABASE"
+        NEW_DATABASE_FILE="${DATABASE_FILE%.db}_${DATA_SIZE}.db"
+        mv "$DATABASE_FOLDER_TEMP/$DATABASE_FILE" "$DATABASE_FOLDER_TEMP/$NEW_DATABASE_FILE"
+        DATABASE_FILE="$NEW_DATABASE_FILE"
 
         # Compress database
-        echo "Compressing database: $DATABASE..."
-        zstd -19 -f "$DATABASE" -o "$DATABASE.zst"
-        echo "Compressed database: $DATABASE.zst"
-        echo "Database size: "$(du -h "$DATABASE" | cut -f1)" (compressed: "$(du -h "$DATABASE.zst" | cut -f1) ")"
-        rm "$DATABASE"
+        echo "Compressing database: $DATABASE_FILE..."
+        zstd -19 -f "$DATABASE_FOLDER_TEMP/$DATABASE_FILE" -o "$DATABASE_FOLDER/$DATABASE_FILE.zst"
+        echo "Database size: "$(du -h "$DATABASE_FOLDER_TEMP/$DATABASE_FILE" | cut -f1)" (compressed: "$(du -h "$DATABASE_FOLDER/$DATABASE_FILE.zst" | cut -f1) ")"
+        rm "$DATABASE_FOLDER_TEMP/$DATABASE_FILE"
         # Propagate exit status of duc from this subshell to pipeline.
         exit $status
     } 2>&1 | tee -a "$LOG_FILE"
